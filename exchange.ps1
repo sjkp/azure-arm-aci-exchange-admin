e2a91c96db3b55ca7cbc713d087451af2dcc2c66
Simon J.K. Pedersen <mail@sjkp.dk>
2019-01-17 23:13:01 +0100

    initial

---
+++ exchange.ps1
@@ -0,0 +1,12 @@
+﻿$PlainPassword = $env:exchangeAdminPassword
+$SecurePassword = $PlainPassword | ConvertTo-SecureString -AsPlainText -Force
+
+$UserName = $env:exchangeAdminUser
+
+$cred = New-Object System.Management.Automation.PSCredential -ArgumentList $UserName, $SecurePassword
+
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/ -Credential $cred -Authentication Basic -AllowRedirection
+
+Import-PSSession $Session -DisableNameChecking
+
+New-TransportRule -Name "no scripted 2" -From $env:mailbox -RejectMessageReasonText "No no can do"
